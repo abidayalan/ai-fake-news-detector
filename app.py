0c76aed1b2f3bb034e6cd4bf3b065265e91add29
Abinaya D <abidayalan1611@gmail.com>
2026-04-09 20:51:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open("model.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    news = request.form["news"]
+
+    vect = vectorizer.transform([news])
+    prediction = model.predict(vect)
+
+    return render_template("index.html", result=prediction[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
